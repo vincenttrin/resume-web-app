1e9476aaee16b45b7fd473af14707ebc61d6f6ed
vincenttrin <vince.trinh80@gmail.com>
2025-09-19 00:09:14 -0500

    moved jobcard.js and professional experiences into existing components folder.

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './Resume.css';
 import Header from '../../components/Header';
-import ProfessionalExperiences from './ProfessionalExperiences';
+import ProfessionalExperiences from '../../components/ProfessionalExperiences';
+import JobCard from '../../components/JobCard';
 
 export default function Resume() {
   const [isOpen, setIsOpen] = useState([false, false]); // Assuming you have two jobs
@@ -20,30 +21,14 @@ export default function Resume() {
   const sortedJobs = [...jobsList].sort((a, b) => b.id - a.id);
 
 
-  
+
   return (
     <>
       <Header />
       <div className="professional-experience">
         <h2>Professional Experience</h2>
         {sortedJobs.map((job, index) => (
-          <div key={job.id} className="job-entry">
-            <div className="job-header" onClick={() => toggleDropdown(index)}>
-              <h3>{job.title}</h3>
-              <p>{job.company}</p>
-              <p>{job.dates}</p>
-            </div>
-            {isOpen[index] && (
-              <div className="job-details">
-                <p>Location: {job.location}</p>
-                <ul>
-                  {job.responsibilities.map((resp, i) => (
-                    <li key={i}>{resp}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </div>
+          <JobCard key={job.id} job={job} isOpen={isOpen[index]} toggleDropdown={() => toggleDropdown(index)} />
         ))}
       </div>
     </>
